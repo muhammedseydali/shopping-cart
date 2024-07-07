525ea0a187892c9988bc300a0acb18c679f0d8f6
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2024-07-07 17:44:57 +0530

    orders views updated

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderedItem
+from products.models import Products
 
 # Create your views here.
-def cart(request):
-    return render(request, 'cart.html')
+
+def show_cart(request):
+    user = request.user
+    customer = user.customer_profile
+    cart_obj,created = Order.objects.get_or_create(owner=customer, order_status = Order.CART_STAGE)
+    context = {'cart':cart_obj}
+    return render(request, 'cart.html', context)
+
+
+def add_cart(request):
+    if request.POST:
+        user = request.user
+        customer = user.customer_profile
+        print(customer, 'customer is here')
+        quantity = int(request.POST.get('quantity'))
+        product_id = request.POST('product_id')
+        cart_obj = Order.objects.get_or_create(owner=customer, order_status=Order.CART_STAGE)
+        products = Products.objects.get(pk=product_id)
+
+        ordered_item, created = OrderedItem.objects.get_or_create(product_id=products, owner = cart_obj)
+        if created:
+            ordered_item.quantity = quantity
+            ordered_item.save()
+        else:
+            ordered_item.quantity + ordered_item.quantity+quantity
+            ordered_item.save()
+    return redirect('cart')
